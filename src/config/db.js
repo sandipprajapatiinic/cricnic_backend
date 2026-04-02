d7c853099514203e641696a04c0f1f22f9704e62
sandip.prjapati <sandip.prajapati@indianic.com>
2026-04-02 13:59:36 +0530

    db url changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 async function connectDb() {
-  const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cricnic';
+  //const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cricnic';
+  const uri = process.env.MONGODB_URI || 'mongodb+srv://cricnic:GEqbyQ2GujOVZoC8@cluster0.obq2pwg.mongodb.net/cricnic_db';
   mongoose.set('strictQuery', true);
   await mongoose.connect(uri);
   return mongoose.connection;
